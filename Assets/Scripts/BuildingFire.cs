ab79d3c04afc5e4513a2ad62df2ba68256f1ba76
jday151r <j.day151r@gmail.com>
2019-09-11 17:58:08 -0700

    Building fire

---
+++ Assets/Scripts/BuildingFire.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingFire : MonoBehaviour
+{
+    public ParticleSystem[] fireEmitters;
+    [Range(0, 1)]
+    public float fireIntensity;
+    public float maxEmission;
+    
+    void Update()
+    {
+        for (int i = 0; i < fireEmitters.Length; i++)
+        {
+            ParticleSystem.EmissionModule emission = fireEmitters[i].emission;
+            emission.rateOverTime = fireIntensity * maxEmission;
+        }
+    }
+}
